1893e106c82a3396febf26e12114002766eacc8f
plxgxx <93287705+plxgxx@users.noreply.github.com>
2022-09-09 11:53:33 +0300

    Add files via upload

--- ticket.py
+++ ticket.py
@@ -11,31 +11,39 @@ def ticket_maker(start, end):
     for itm in range(start, end):
         a1 = f'{itm:06}'
         if int(a1[0]) + int(a1[1]) + int(a1[2]) == int(a1[3]) + int(a1[4]) + int(a1[5]):
-            #print(f"The happy ticket is: {itm}") Щоб продивитись обрахунки
+            print(f"The happy ticket is: {itm}") #Щоб продивитись обрахунки
             with open("a1.txt", "a") as res:
                 res.write(a1)
 
 if __name__  == "__main__":
 
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
+
+    print("Started the process of threading")
     start_time1 = time.perf_counter()
-    logging.info("Main    : before creating thread")
-    x = threading.Thread(target=ticket_maker(500000, 511155), args=(1,))
-    logging.info("Main    : before running thread")
-    x.start()
-    logging.info("Main    : wait for the thread to finish")
-    # x.join()
-    logging.info("Main    : all done")
+
+    x1 = threading.Thread(target=ticket_maker(1, 200000), args=(1,))
+    x1.start()
+
+    x2 = threading.Thread(target=ticket_maker(200000, 500000), args=(1,))
+    x2.start()
+
+    x3 = threading.Thread(target=ticket_maker(500000, 700000), args=(1,))
+    x3.start()
+
+    x4 = threading.Thread(target=ticket_maker(700000, 900000), args=(1,))
+    x4.start()
+
+    x5 = threading.Thread(target=ticket_maker(900000, 999999), args=(1,))
+    x5.start()
 
     end_time1 = time.perf_counter()
     print(f"Threading ended in {end_time1 - start_time1:0.4f} seconds")
 
+    print("Started the process of multiprocessing")
     start_time2 = time.perf_counter()
 
     procs = []
-    proc1 = Process(target=ticket_maker(1, 200000))  # instantiating without any argument
+    proc1 = Process(target=ticket_maker(1, 200000), args=(1,))  # instantiating without any argument
     procs.append(proc1)
     proc1.start()
 
@@ -44,7 +52,7 @@ if __name__  == "__main__":
     procs.append(proc2)
     proc2.start()
 
-    proc3 = Process(target=ticket_maker(500000, 700000))
+    proc3 = Process(target=ticket_maker(500000, 700000), args=(1,))
     procs.append(proc3)
     proc3.start()
 
