2339b1c00b9dfaaa8752028bd14a693ded048c52
plxgxx <93287705+plxgxx@users.noreply.github.com>
2022-09-09 11:37:13 +0300

    Add files via upload

--- ticket.py
+++ ticket.py
@@ -4,11 +4,14 @@ import time
 import threading
 from multiprocessing import Process
 
+
+
 def ticket_maker(start, end):
+    print(f'args: {start}, {end}')
     for itm in range(start, end):
         a1 = f'{itm:06}'
         if int(a1[0]) + int(a1[1]) + int(a1[2]) == int(a1[3]) + int(a1[4]) + int(a1[5]):
-            print(f"The happy ticket is: {itm}")
+            #print(f"The happy ticket is: {itm}") Щоб продивитись обрахунки
             with open("a1.txt", "a") as res:
                 res.write(a1)
 
@@ -30,18 +33,28 @@ if __name__  == "__main__":
     print(f"Threading ended in {end_time1 - start_time1:0.4f} seconds")
 
     start_time2 = time.perf_counter()
-    start = 1
-    ending_point = 999999
+
     procs = []
-    proc = Process(target=ticket_maker(500000, 510000))  # instantiating without any argument
-    procs.append(proc)
-    proc.start()
+    proc1 = Process(target=ticket_maker(1, 200000))  # instantiating without any argument
+    procs.append(proc1)
+    proc1.start()
 
     # instantiating process with arguments
-    for start in range(ending_point):
-        proc = Process(target=ticket_maker(500000, 510000), args=(start,))
-        procs.append(proc)
-        proc.start()
+    proc2 = Process(target=ticket_maker(200000, 500000), args=(1,))
+    procs.append(proc2)
+    proc2.start()
+
+    proc3 = Process(target=ticket_maker(500000, 700000))
+    procs.append(proc3)
+    proc3.start()
+
+    proc4 = Process(target=ticket_maker(700000, 900000), args=(1,))
+    procs.append(proc4)
+    proc4.start()
+
+    proc5 = Process(target=ticket_maker(900000, 999999), args=(1,))
+    procs.append(proc5)
+    proc5.start()
 
     # complete the processes
     for proc in procs:
